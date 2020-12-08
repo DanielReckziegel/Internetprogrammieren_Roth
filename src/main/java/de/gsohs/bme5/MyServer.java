2c178308d7fdcc434fef4e2ab761b00274055533
Daniel_Reckziegel <reckziegelda74985@th-nuernberg.de>
2020-12-08 17:25:18 +0100

    HTML Startseite, Bilder und Podcast1 hinzugefügt. persitence.xml hinzugefügt und Datenbankanbindung hinzugefügt

--- src/main/java/de/gsohs/bme5/MyServer.java
+++ src/main/java/de/gsohs/bme5/MyServer.java
@@ -1,11 +1,15 @@
 package de.gsohs.bme5;
 import io.javalin.Javalin;
+
+import javax.persistence.Persistence;
 import java.util.List;
 import java.util.Map;
 
 public class MyServer {
     public static void main(String[] args) {
+        Persistence.createEntityManagerFactory("podlike");
         Javalin javalin = Javalin.create().start(8082);
+        javalin.config.addStaticFiles("static");
         javalin.get("/hello", ctx -> {
             String name = ctx.queryParam("name", "World");
             ctx.result("Hello "+name+"!");
@@ -28,5 +32,14 @@ public class MyServer {
         javalin.get("/login", ctx -> {
             ctx.render("templates/layout.html");
         });
+
+        javalin.get("/Podcast1", ctx -> {
+            ctx.render("templates/Podcast1.html");
+        });
+
+        javalin.get("/home", ctx -> {
+            ctx.render("templates/Startseite.html");
+        });
+
     }
 }
